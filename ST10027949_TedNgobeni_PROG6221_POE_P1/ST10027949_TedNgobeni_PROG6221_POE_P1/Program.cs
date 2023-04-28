cb0b42aa92166b1b9f19cf433e3d52d0feb3b18a
ST10027949 <104318031+ST10027949@users.noreply.github.com>
2023-04-28 11:42:05 +0200

    Initial Commit
    Created classes with get and set methods. Main method and constructor for the external class.

---
+++ ST10027949_TedNgobeni_PROG6221_POE_P1/ST10027949_TedNgobeni_PROG6221_POE_P1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ST10027949_TedNgobeni_PROG6221_POE_P1
+{
+    //Creation of the recipe class that willl hold the methods.
+    class Recipe
+    {
+        public int NumberOfIngredients { get; private set; }
+        public string[] IngredientNames { get; private set; }
+        public double[] IngredientQuantities { get; private set; }
+        public string[] MeasurementUnit { get; private set; }
+        public int NumberOfSteps { get; private set; }
+        public string[] StepDescriptions { get; private set; }
+
+      
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recipe recipe = new Recipe();
+          
+        }
+    }
+
+}
+
+
